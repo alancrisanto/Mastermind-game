444e67fdf3eed4ee9fb54afb8c7900f986dc5561
tatendafmukaro <tatendafmukaro@outlook.com>
2021-10-20 18:47:24 +0200

    arbiterclass-comments

---
+++ arbiter.py
@@ -0,0 +1,11 @@
+from player import Player
+
+class Arbiter():
+     """Responsible for taking player's guess, checking againnst the code, returning hint
+    Stereotype:
+        Information Holder
+
+    Attributes:
+       
+    """
+    pass
