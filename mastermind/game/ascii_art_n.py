776974777fef1aae674dfc081701e69ee8a2411e
TiannaDeSpain <71282952+TiannaDeSpain@users.noreply.github.com>
2021-10-22 10:24:51 -0600

    Tianna's Class- AsciiArt

--- mastermind/game/ascii_art_n.py
+++ mastermind/game/ascii_art_n.py
@@ -1,11 +1,42 @@
-class AsciiArt():
-    """ to be written by Tianna
-    """
+class AsciiArt:
+    """Responsible for displaying ASCII art and user-friendly messages."""
+    
     def __init__(self) -> None:
         pass
 
-    def smiley(self):
-        """
-        """
-        smiley = "(^_^)"
-        return smiley
+    def good_job():
+        #to be used when the guess is correct
+        print("(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ \n You got it, great job! \n")
+
+    def whoops():
+        #to be used when the guess contains no right numbers
+        print("¯\_(ツ)_/¯ \n Don't give up! \n")
+
+    def game_over():
+        #for the end of game message
+        print("┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼ \n"
+            "███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀ \n"
+            "██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼ \n"
+            "██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀ \n"
+            "██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼ \n"
+            "███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄ \n"
+            "┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼ \n"
+            "███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼ \n"
+            "██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼ \n"
+            "██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼ \n"
+            "██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼ \n"
+            "███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄ \n"
+            "\n")
+
+
+# for testing
+def main():
+    # Create a AsciiArt object
+    art = AsciiArt
+    art.good_job()
+    art.whoops()
+    art.game_over()
+
+
+if __name__ == "__main__":
+    main()
